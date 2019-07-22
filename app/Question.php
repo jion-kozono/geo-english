140b46ec98411479beedf58443c18931dc62fa00
小園志旺 <zeon66190329@gmail.com>
2019-07-22 17:58:43 +0900

    2019/07/22 Question

---
+++ app/Question.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $fillable = [
+        'id',
+        'question_grade',
+        'question',
+        'option_1',
+        'option_2',
+        'option_3',
+        'option_4',
+        'answer',
+    ];
+}
