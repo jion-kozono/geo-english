09a826024090f1b0fa8382bef9816a410b4558d7
小園志旺 <zeon66190329@gmail.com>
2019-07-22 17:56:36 +0900

    2019/07/22 userinfo

---
+++ app/UserInfo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserInfo extends Model
+{
+    protected $table = 'user_info';
+    // protected $fillable = [
+    //     'id',
+    //     'user_name',
+    //     'password',
+    //     'user_email',
+    // ];
+}
